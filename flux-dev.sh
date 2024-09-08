17093d49a9d1dd7f5976d986fdc2b057dc3c5eb4
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-09-08 22:55:15 +0200

    Add dependencies and clone additional repositories
    This commit adds the installation of the "bitsandbytes" package and clones several additional repositories: "ComfyUI_bitsandbytes_NF4", "ComfyUI-Impact-Pack", and "WAS Node Suite". It also installs the requirements for the "WAS Node Suite" repository.

--- flux-dev.sh
+++ flux-dev.sh
@@ -4,9 +4,18 @@ cd ComfyUI
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+pip install bitsandbytes
 cd custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git
 echo "Manager cloned"
+git clone https://github.com/comfyanonymous/ComfyUI_bitsandbytes_NF4.git
+echo "Bits and Bytes NF4 cloned"
+git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack.git
+echo "Impact Pack cloned"
+git clone https://github.com/WASasquatch/was-node-suite-comfyui.git
+echo "WAS Node Suite cloned"
+pip install -r was-node-suite-comfyui/requirements.txt
+echo "WAS Node Suite requirements installed"
 cd ..
 cd models/checkpoints
 wget "https://huggingface.co/lllyasviel/flux1-dev-bnb-nf4/resolve/main/flux1-dev-bnb-nf4-v2.safetensors?download=true" -O flux1-dev-bnb-nf4-v2.safetensors
