5ef9b6d3530aefc9786e5a2536421b73d392baf2
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-09-26 12:25:50 +0200

    Add venv setup and dependency installation to flux-dev.sh

--- flux-dev.sh
+++ flux-dev.sh
@@ -4,7 +4,6 @@ cd ComfyUI
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-pip install bitsandbytes
 cd custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git
 echo "Manager cloned"
